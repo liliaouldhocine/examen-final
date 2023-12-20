8a883fa7c43c9607b04c5a98d3282ed82d12adb7
Clara Quintela <claraquintela@gmail.com>
2023-12-20 15:38:13 -0500

    Changer nom et prénom

--- examen.js
+++ examen.js
@@ -1,6 +1,6 @@
 let getNomComplet = () => {
-  let monNom = "Ould Hocine";
-  let monPrenom = "Lilia";
+  let monNom = "QuintelaDeAlmeida";
+  let monPrenom = "Clara";
   return {
     nom: monNom,
     prenom: monPrenom,
